46e5cba7fcf950add8d9dcdc7c9271d05041c0d2
daweizh <dawei-msn@hotmail.com>
2018-09-29 16:54:52 +0800

    qq

---
+++ junior/repecharge/2016/pencil/pencil.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int n,m;
+int minimum,package,price;
+
+int main(){
+	freopen("pencil.in","r",stdin);
+	freopen("pencil.out","w",stdout);
+	
+	cin >> n;
+	minimum = n * 10000;
+	
+	for(int i=0;i<3;i++){
+		cin >> package >> price;
+		
+		if(n%package==0){
+			m = n/package * price;
+		}else{
+			m = (n/package + 1) * price;
+		}
+		
+		if(m < minimum){
+			minimum = m;
+		}
+	}
+
+	cout << minimum << endl;
+	
+	return 0;
+}
